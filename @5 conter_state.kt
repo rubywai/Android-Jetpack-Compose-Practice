67f223b78924d8db6dc847d9f39e4ad4ed41fac2
Ruby Wai <62864134+rubywai@users.noreply.github.com>
2022-06-16 23:05:36 +0630

    Create @5 conter_state.kt

---
+++ @5 conter_state.kt
@@ -0,0 +1,52 @@
+package com.example.composeapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyText()
+        }
+    }
+}
+
+@Composable
+fun MyText() {
+    var name by rememberSaveable() {
+        mutableStateOf(0)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(name.toString(), fontSize = 30.sp, fontWeight = FontWeight.Bold)
+
+        Button(onClick = {
+            name ++
+        },
+        ) {
+           Text("+")
+        }
+    }
+}
+
