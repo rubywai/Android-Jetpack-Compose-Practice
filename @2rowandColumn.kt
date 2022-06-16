74ee4284ece8b99e475accd49d056b63bb260a53
Ruby Wai <62864134+rubywai@users.noreply.github.com>
2022-06-16 22:23:16 +0630

    Create @2rowandColumn.kt

---
+++ @2rowandColumn.kt
@@ -0,0 +1,51 @@
+package com.example.composeapplication
+
+import android.graphics.Color.red
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.example.composeapplication.ui.theme.ComposeApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           Column(
+               modifier  = Modifier
+                   .fillMaxSize()
+                   .background(Color.Red)
+
+                       ,
+               horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.SpaceAround) {
+               Text("Hello",color = Color.White, fontSize = 24.sp)
+               Text("Hello",color = Color.White, fontSize = 16.sp)
+               Row(
+                   modifier = Modifier.fillMaxWidth(),
+                   horizontalArrangement = Arrangement.SpaceAround,
+                   verticalAlignment = Alignment.CenterVertically
+
+               ) {
+                   Text("Hi")
+                   Text("Hi")
+               }
+           }
+        }
+    }
+}
+
